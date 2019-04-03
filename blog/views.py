76d0048ca0deea02a68598907717cdffc9001354
Hannah Marto <hmarto2@lsu.edu>
2019-04-03 15:26:07 -0500

    configuring for heroku

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    # This is a QuerySet that returns all the posts ordered by the publish date
+    posts = Post.objects.order_by('published_date')
+    # Pass the posts to the template
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
